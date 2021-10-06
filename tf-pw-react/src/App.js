6041e088dc527e85468081f19c72bf7954935653
Mokenuf <mokenuf@me.com>
2021-10-06 17:41:07 -0300

    todo-roto

--- tf-pw-react/src/App.js
+++ tf-pw-react/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div class="navbar"></div>
+        <div class="hero-image">
+            <div class="hero-text">
+              <h1 class="pGrande">Gremio de los Cazarrecompensas</h1>
+              <p>Tu problema, nuestro trabajo</p>
+              <button>Aplicar</button>
+            </div>
+          </div>
+        <div class="content">
+          <p class="pGrande">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, accusantium ad nesciunt soluta voluptates architecto velit perspiciatis sed laborum, tempore expedita rerum magni at sunt, eos debitis animi maiores? Ut.</p>
+        </div>
+        <div class="content2">
+          <p class="hero-text, pGrande">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, accusantium ad nesciunt soluta voluptates architecto velit perspiciatis sed laborum, tempore expedita rerum magni at sunt, eos debitis animi maiores? Ut.</p>
+        </div>
+        <div class="content">
+          <p class="pGrande">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, accusantium ad nesciunt soluta voluptates architecto velit perspiciatis sed laborum, tempore expedita rerum magni at sunt, eos debitis animi maiores? Ut.</p>
+        </div>
     </div>
   );
 }
