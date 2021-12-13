546b9e15f9810dba6059434bd51226f7d3ef9d6d
Rodrigo Prego <89752636+Withdrow@users.noreply.github.com>
2021-12-13 20:50:54 -0300

    CreateAccount

--- tf-pw-react/src/Coponents/Jobs.js
+++ tf-pw-react/src/Coponents/Jobs.js
@@ -42,8 +42,13 @@ const Jobs = () => {
 
     const searchJobs = (e) => {
         e.preventDefault()
-        httpGet('api/jobs/?name='+name2)
-            .then((res) => setJobs(res.data))
+        if (name2){
+            httpGet('api/jobs/?name='+name2)
+                .then((res) => setJobs(res.data))}
+        else{
+            httpGet('api/jobs/?reward='+reward2)
+                .then((res) => setJobs(res.data))
+        }
     }
 
     const createJob = (e) => {
